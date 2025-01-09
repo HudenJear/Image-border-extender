abcc4ab50082d45a5b21e42d1cb52b75caf2b446
Huden <57908972+HudenJear@users.noreply.github.com>
2025-01-09 17:32:20 +0800

    Add new method codes and images

--- add_bd.py
+++ add_bd.py
@@ -1,8 +1,57 @@
-from PIL import Image,ImageDraw,ImageFont
+from PIL import Image,ImageDraw,ImageFont,ImageOps
 import  os,glob
 tgt='./imgdone'
 tgt_size=2400
 ratio=0.9
+border_size=int(0.03*tgt_size)
+border_color='black'
+exterior=int(0.05*tgt_size)
+infor_area=int(0.15*tgt_size)
+
+def process_one_image(img_path):
+
+    img = Image.open(img_path)
+
+
+    # 计算要将原始图片粘贴到白色背景图上的位置,rotate or not
+    rota=True if img.width < img.height * 0.95 else False
+
+    if rota:
+        img=img.rotate(90)
+
+    # 计算新的图像尺寸
+    new_width = tgt_size
+    new_height = int(img.height * new_width / img.width)
+    # 重新缩放原始图片
+    img = img.resize((new_width, new_height))
+    # left = int((tgt_size * (1 - ratio)) // 2)
+
+    # calculate bg size
+    background = Image.new('RGB', (tgt_size+2*border_size+2*exterior, new_height+2*border_size+3*exterior+infor_area), (255, 255, 255))
+    # add border 1
+    img= ImageOps.expand(img, border=border_size, fill=border_color)
+
+    # 将原始图片粘贴到白色背景图上
+    background.paste(img, (exterior, exterior))
+    draw = ImageDraw.Draw(background)
+
+    # add text
+    font = ImageFont.truetype("arial.ttf", 80)
+    text = "Olympus OM-30 Type.1985\n\nOM-System Zuiko.S 50mm F1,4"
+    x, y = exterior, 2 * exterior + new_height + 2*border_size
+    draw.text((x, y), text, fill=(0, 0, 0), font=font)
+    text2 = "\nShot in Somewhere on the earth."
+
+    # add logo
+    logo_img=Image.open('logos/Olympus.png')
+    logo_height=infor_area*0.8
+
+
+    if rota:
+        img=img.rotate(270)
+    # 保存最终结果
+    return background.save(os.path.join(tgt, os.path.splitext(os.path.split(img_path)[1])[0] + f".webp"))
+
 
 if not os.path.exists(tgt):
     os.mkdir(tgt)
@@ -15,40 +64,4 @@ for suf in ['png','jpeg','jpg','PNG']:
 print(f'Images need to be processed: {img_all}')
 for indx in range(len(img_all)):
     print("\rProcessing line {}/{}...".format(indx+1, len(img_all)), end='', flush=True)
-    img_path=img_all[indx]
-    img = Image.open(img_path)
-    background = Image.new('RGB', (tgt_size, tgt_size), (255, 255, 255))
-
-    # 计算要将原始图片粘贴到白色背景图上的位置
-    if img.width>img.height*0.95:
-        # 计算新的图像尺寸
-        new_width = int(tgt_size*ratio)
-        new_height = int(img.height * new_width / img.width)
-        # 重新缩放原始图片
-        img = img.resize((new_width, new_height))
-        left = int((tgt_size *(1-ratio)) // 2)
-        top = 100
-        # 将原始图片粘贴到白色背景图上
-        background.paste(img, (left, top))
-        draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("arial.ttf", 100)
-        text = "Olympus OM-30 Type.1985\nOM-System Zuiko.S 50mm F1,4\nShot in Somewhere on the earth."
-        x, y = left, 2*top+new_height
-        draw.text((x, y), text, fill=(0, 0, 0), font=font)
-    else:
-        new_height = int(tgt_size*ratio)
-        new_width = int(img.width * new_height / img.height)
-        # 重新缩放原始图片
-        img = img.resize((new_width, new_height))
-        top = int((tgt_size *(1-ratio)) // 2)
-        left = 100
-        # 将原始图片粘贴到白色背景图上
-        background.paste(img, (left, top))
-        draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("arial.ttf", 100)
-        text = "Olympus OM-30 Type.1985\nOM-System Zuiko.S 50mm F1,4\nShot in Somewhere on the earth."
-        x, y = 100, 50
-        draw.text((x, y), text, fill=(0, 0, 0), font=font)
-
-    # 保存最终结果
-    background.save(os.path.join(tgt, os.path.splitext(os.path.split(img_path)[1])[0]+f".webp"))
+    process_res = process_one_image(img_all[indx])
