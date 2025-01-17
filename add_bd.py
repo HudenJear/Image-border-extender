0e7117f3f717978b9fe1a29bf82f6a0cb159e119
Huden <57908972+HudenJear@users.noreply.github.com>
2025-01-17 14:59:08 +0800

    new element added 2

--- add_bd.py
+++ add_bd.py
@@ -1,4 +1,6 @@
 from PIL import Image,ImageDraw,ImageFont,ImageOps
+from PIL.ExifTags import TAGS
+import piexif
 import  os,glob
 from color_extract import  extract_main_colors
 import numpy as np
@@ -7,11 +9,21 @@ src='./imgtoprocess'
 tgt='./imgdone'
 tgt_size=2400
 ratio=0.9
-border_size=int(0.015*tgt_size)
+border_size=int(0.01*tgt_size)
 border_color='black'
-exterior=int(0.05*tgt_size)
+exterior=int(0.03*tgt_size)
 infor_area=int(0.15*tgt_size)
 font_size=int(tgt_size*0.03)
+
+
+
+# suppli_info="Kodak Vision3 5219 500T 120"
+# suppli_info="Kodak Vision3 5207 250T 120"
+# suppli_info="Kodak EktarChrome 5294 100D 135"
+suppli_info="FUJICHROME Velvia 100 Daylight 120"
+# suppli_info="FUJICHROME Provia 100f Daylight 120"
+# suppli_info=None
+
 
 text_dict={
     'hassel_CF60':["Hasselblad 500CM Type.1990s\n\nCarl Zeiss CF 60mm F3,5",'logos/hassel.jpg'],
@@ -21,15 +33,28 @@ text_dict={
     'olym_2848': ["Olympus OM-30\n\nZuiko S Auto-Zoom 28-48mm F4", 'logos/Olympus.jpg'],
     'mamiya_six': ["Mamiya-Six Type.K-1953\n\nOlympus D.Zuiko F.C. 75mm F3.5 Sekorsha", 'logos/mamiya.jpg'],
     'minolta': ["Minolta Hi-Matic E \n\nRokkor-QF 40mm F1,7", 'logos/Minolta.jpg'],
+    'auto_detect':['',''],
 
 }
-# text = "Olympus OM-30 Type.1985\n\nOM-System Zuiko.S 50mm F1,4"
-# text = "Minolta Hi-Matic E Type.1982\n\nRokkor-QF 40mm F1,7"
+logo_dict={
+'hassel_CF60':'logos/hassel.jpg',
+    'hassel_CF150': 'logos/hassel.jpg',
+    'olym_50':'logos/Olympus.jpg',
+    'olym_135': 'logos/Olympus.jpg',
+    'olym_2848': 'logos/Olympus.jpg',
+    'mamiya_six': 'logos/mamiya.jpg',
+    'minolta':'logos/Minolta.jpg',
+    'SONY':'logos/Sony-Alpha-Logo.png',
+    'Panasonic': 'logos/Panasonic-Lumix-Logo.jpg'
 
-# logo_path='logos/Olympus.jpg'
-# logo_path='logos/Minolta.jpg'
-# logo_path='logos/mamiya.jpg'
-# logo_path='logos/mamiya2.jpg'
+}
+
+def initializing_directories():
+    for dir_name in list(text_dict.keys()):
+        if not os.path.exists(os.path.join(src,dir_name)):
+            os.mkdir(os.path.join(src,dir_name))
+    if not os.path.exists(tgt):
+        os.mkdir(tgt)
 
 def rotate_image_90_no_crop(image_data,reverse=False):
     # 打开图像
@@ -55,9 +80,62 @@ def rotate_image_90_no_crop(image_data,reverse=False):
 
     return cropped_image
 
+
 def process_one_image(img_path,text,logo_file):
 
     img = Image.open(img_path).convert('RGB')
+    suppli_line=suppli_info
+    # if the image is auto-detected, the exif will be extracted
+    if 'auto_detect' in img_path and text=='':
+        # exif=img.info['exif']
+        exif_data = img.getexif()
+        camera_mk=None
+        camera_m=None
+        if exif_data:
+            for tag, value in exif_data.items():
+                tag_name = TAGS.get(tag, tag)
+                # print(f"{tag_name}: {value}")
+                if tag_name=="Make":
+                    camera_mk=value
+                elif tag_name=='Model':
+                    camera_m=value
+
+        if camera_mk and camera_m:
+            text = camera_mk + ' '+ camera_m+'\n\n'
+            logo_file=logo_dict[camera_mk]
+            try:
+                exif_dict = piexif.load(img.info['exif'])
+                # print(exif_dict['Exif'].keys())
+                # for element in dir(piexif.ExifIFD):
+                #     # 排除内置属性和方法
+                #     if not element.startswith('__'):
+                #         value = getattr(piexif.ExifIFD, element)
+                #         if value in exif_dict['Exif'].keys():
+                #             print(f"{element}: {value}")
+                #             # print(f"{element}: {value}: {exif_dict['Exif'][value]}")
+                focal_length = exif_dict['Exif'][piexif.ExifIFD.FocalLength]
+                # print(focal_length)
+                F_value=exif_dict['Exif'][piexif.ExifIFD.FNumber]
+                # print(F_value)
+                ISO_value=exif_dict['Exif'][piexif.ExifIFD.ISOSpeedRatings]
+                # print(str(exif_dict['Exif'][piexif.ExifIFD.LensModel]))
+                text=text+exif_dict['Exif'][piexif.ExifIFD.LensModel].decode('utf-8')
+                # camera_m=exif_data['Make']
+                suppli_line='Focal Length: '+str(int(focal_length[0]/focal_length[1]))+'mm    '+'Aperture: F'+str(F_value[0]/F_value[1]).replace('.',',')+'    '+'ISO/Din: '+str(ISO_value)
+
+
+
+                # print(text)
+                # print(logo_file)
+                # print(suppli_line)
+
+            except:
+                print(f"Exif detected broken for one image in the auto detect dictionary:{img_path}")
+
+
+        else:
+            print(f"No exif detected for one image in the auto detect dictionary:{img_path}")
+            return
     # print(img.height,img.width)
 
     # 计算要将原始图片粘贴到白色背景图上的位置,rotate or not
@@ -92,19 +170,19 @@ def process_one_image(img_path,text,logo_file):
     logo_img=logo_img.resize((int(logo_img.width*logo_height/logo_img.height),int(logo_height)))
     background.paste(logo_img,(int(tgt_size+2*border_size+exterior-logo_img.width*logo_height/logo_img.height),int(new_height+2*border_size+2*exterior)))
     draw = ImageDraw.Draw(background)
+
     # add text 1 the camera
     font = ImageFont.truetype("arial.ttf", font_size)
-    posi = (exterior, 2 * exterior + new_height + 2*border_size)
+    posi = (int(exterior*1.01), 2 * exterior + new_height + 2*border_size)
     text_1=text.split('\n\n')[0]
     draw.text(posi, text_1, fill=(0, 0, 0), font=font)
-    # mkae it bold
-    bold_offset = 1
+    bold_offset = 1 # make it bold
     for offset in [(0, 0), (bold_offset, 0), (0, bold_offset), (bold_offset, bold_offset)]:
         draw.text((posi[0] + offset[0], posi[1] + offset[1]), text_1, font=font, fill=(0, 0, 0))
 
-    # add text 1 the lens
+    # add text 2 the lens
     font = ImageFont.truetype("arial.ttf", int(font_size*0.9))
-    posi = (exterior, 2 * exterior + new_height + 2 * border_size+1.8*font_size)
+    posi = (int(exterior*1.01), 2 * exterior + new_height + 2 * border_size+1.6*font_size)
     text_2 = text.split('\n\n')[1]
     draw.text(posi, text_2, fill=(0, 0, 0), font=font)
     # text2 = "\nShot in Somewhere on the earth."
@@ -112,17 +190,20 @@ def process_one_image(img_path,text,logo_file):
     # add main_color
     main_c=extract_main_colors(img,num_colors=4)
     color_image = np.zeros((int(0.8* font_size), int(15*font_size), 3), dtype=int)
-
     block_width = color_image.shape[1] // len(main_c)
-
     for i, color in enumerate(main_c):
         color_image[:, i * block_width:(i + 1) * block_width] = color
     color_pad=Image.fromarray(color_image.astype('uint8'))
-    posi_mc=(exterior, int(2 * exterior + new_height + 2 * border_size+3.4*font_size))
+    posi_mc=(int(exterior*1.01), int(2 * exterior + new_height + 2 * border_size+3.0*font_size))
     background.paste(color_pad,posi_mc)
 
-    # rotate back and save
+    # add supplementary_line in the last line
+    if suppli_line:
+        font = ImageFont.truetype("arial.ttf", int(font_size * 0.8))
+        posi = (int(exterior*1.01), 2 * exterior + new_height + 2 * border_size + 4.2 * font_size)
+        draw.text(posi, suppli_line, fill=(80, 80, 80), font=font)
 
+    # rotate back and save
     if rota:
         background=rotate_image_90_no_crop(background,reverse=False)
     dir_p=os.path.split(os.path.split(img_path)[0])[1]
@@ -133,20 +214,24 @@ def process_one_image(img_path,text,logo_file):
 
 
 if __name__=='__main__':
-    if not os.path.exists(tgt):
-        os.mkdir(tgt)
+    initializing_directories()
+    # if not os.path.exists(tgt):
+    #     os.mkdir(tgt)
 
     # 加载原始图片
 
     dir_list=os.listdir(src)
     for dir_name in dir_list:
         if os.path.isdir(os.path.join(src,dir_name)) and dir_name in text_dict:
+            # if dir_name=='auto_detect':
+            #     pass
+            # else:
             text_line,logo_path=text_dict[dir_name]
             img_all = []
             for suf in ['png','jpeg','jpg','PNG','tif']:
                 img_all.extend(glob.glob(os.path.join(src,dir_name,"*."+suf)))
 
-            print(f'Images need to be processed: {img_all}')
+            print(f'Images need to be processed in {dir_name} : {img_all}')
             for indx in range(len(img_all)):
                 print("\rProcessing line {}/{}...".format(indx+1, len(img_all)), end='', flush=True)
                 process_res = process_one_image(img_all[indx],text=text_line,logo_file=logo_path)
